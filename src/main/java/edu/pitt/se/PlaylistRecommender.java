05d10223e63089b012ebf363ba4253436a4369f8
CSH63 <143836704+CSH63@users.noreply.github.com>
2025-11-26 13:53:45 -0500

    impliment classifyEnergy
    title

--- src/main/java/edu/pitt/se/PlaylistRecommender.java
+++ src/main/java/edu/pitt/se/PlaylistRecommender.java
@@ -5,8 +5,21 @@ import java.util.List;
 public class PlaylistRecommender {
 
     public static String classifyEnergy(List<Integer> bpms) {
-        // TODO: Implement classifyEnergy()
-        throw new UnsupportedOperationException("Not implemented");
+        int total = 0;
+        int numBpm = 0;
+        for (Integer bpm : bpms) {
+            total += bpm;
+            numBpm++;
+        }
+        int avg = total/numBpm;
+        String returner = "MEDIUM";
+        if (avg >= 140) {
+            returner = "HIGH";
+        }
+        if (avg < 100) {
+            returner = "LOW";
+        }
+        return returner;
     }
 
     public static boolean isValidTrackTitle(String title) {
